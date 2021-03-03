1d225d3d6e632675df7afdd41778354e24c32d9d
LE-COQ Amos <Amos.LE-COQ@cpnv.ch>
2021-03-03 10:35:01 +0100

    porte-avions

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 int main() {
     int nombres [10] [10];
     int ligne,col;
+    int pa1,pa2;
+
+    srand((unsigned) time(NULL));
 
     for (ligne = 0; ligne<=9; ligne++) {
         for (col = 0; col<=9; col++) {
@@ -17,11 +22,11 @@ int main() {
             }
         }
     }
-}
+    pa1=1+rand()%9;
+    pa2=1+rand()%9;
+    nombres[pa1][pa2]=1;
+    for (int i=0;i<=5;i++) {
 
-
-int porte-avions(){
-    
-
+    }
 
 }
